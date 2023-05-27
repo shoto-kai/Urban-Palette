41d9f6a11b80a51e1d06af3896ef90b3aa5c497a
T08Y02 <pon9q9@gmail.com>
2023-05-28 01:05:11 +0900

    #5

--- app/UrbanPalette/Assets/Scenes/scripts/building/Test.cs
+++ app/UrbanPalette/Assets/Scenes/scripts/building/Test.cs
@@ -1,25 +1,73 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+
 
 public class Test : MonoBehaviour
 {
+    //画像リンクから画像をテクスチャにする
+    Texture texture;
+    //テクスチャをマテリアル化するので生成しておく
+    [SerializeField]Material material;
+    //画像リンク
+
     void Start()
     {
         //Debug.Log(name);
-        Material mat = this.GetComponent<Renderer>().material;
+        // Material mat = this.GetComponent<Renderer>().material;
         // マテリアルの色設定に赤色を設定
+        // if (this.tag == "red"){
+        //     mat.color = new Color(1.0f,0.0f,0.0f,1.0f);
+        //     Debug.Log(name);
+        // }
+
+        // if (this.tag == "blue"){
+        //     mat.color = new Color(0.0f,0.0f,1.0f,1.0f);
+        //     Debug.Log(name);
+        // }
+
+        // if (this.tag == "green"){
+        //     mat.color = new Color(0.0f,1.0f,0.0f,1.0f);
+        //     Debug.Log(name);
+        // }
+        StartCoroutine(Connect());
+    }
+
+    private IEnumerator Connect()
+    {
+        string url_1 = "https://res.cloudinary.com/demo/image/upload/sample.jpg";
+        string url_2 = "https://res.cloudinary.com/cld-name/image/upload/l_harlequinmask,w_1.7,g_adv_eyes,fl_region_relative/team.jpg";
+        string url_3 = "https://res.cloudinary.com/demo/image/upload/pasta.jpg";
+        UnityWebRequest www = UnityWebRequestTexture.GetTexture(url_1);   
+
         if (this.tag == "red"){
-            mat.color = new Color(1.0f,0.0f,0.0f,1.0f);
-            Debug.Log(name);
+            www = UnityWebRequestTexture.GetTexture(url_1);               
         }
 
         if (this.tag == "blue"){
-            mat.color = new Color(0.0f,0.0f,1.0f,1.0f);
-            Debug.Log(name);
+            www = UnityWebRequestTexture.GetTexture(url_2);                    
         }
 
         if (this.tag == "green"){
-            mat.color = new Color(0.0f,1.0f,0.0f,1.0f);
-            Debug.Log(name);
+            www = UnityWebRequestTexture.GetTexture(url_3);                    
+        }
+        Material mat = this.GetComponent<Renderer>().material;
+
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError ||www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {  
+            //textureに画像が入るよ
+            texture = ((DownloadHandlerTexture)www.downloadHandler).texture;
+            //textureをマテリアルにセット
+            mat.SetTexture("_MainTex", texture);
+
+            //this.GetComponent<Renderer>().material = material;
         }
     }
 }
