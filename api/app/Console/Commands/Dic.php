f24bb15dc36ec683cfea829ef2235c4bc7e223b7
YuugouOhno <yuugou.ohno@gmail.com>
2023-06-04 06:42:40 +0900

    unity辞書から辞書を作成

--- api/app/Console/Commands/Dic.php
+++ api/app/Console/Commands/Dic.php
@@ -4,29 +4,29 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 
-class MakeDictionary extends Command
+class Dic extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'app:make-dictionary';
+    protected $signature = 'app:dic';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = '地名辞書を作成する';
+    protected $description = 'Command description';
 
     /**
      * Execute the console command.
      */
     public function handle()
     {
-        $MakeDictionary_controller = app()->make('App\Http\Controllers\Api\MakeDictionaryController');
-        $MakeDictionary_controller->dictionary();
+        $Dic_controller = app()->make('App\Http\Controllers\Api\DicController');
+        $Dic_controller->dic();
         return Command::SUCCESS;
     }
 }
--- api/config/apikey.php
+++ api/config/apikey.php
@@ -1,5 +1,6 @@
 <?php
 
 return [
-    'hotpepper_access_token' => env('HOTPEPPER_ACCESS_TOKEN', 'hoge'),
+    'instagram_access_token' => env('INSTAGRAM_ACCESS_TOKEN', 'hoge'),
+    'hotpepper_access_key' => env('HOTPEPPER_ACCESS_KEY', 'hoge'),
 ];
